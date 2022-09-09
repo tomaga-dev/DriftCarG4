abdb1687df45c604cc54a06cf32be037b632ee3e
tomaga <tomaga1080@protonmail.com>
2022-09-09 18:14:36 +0200

    Motor sound volume
    The motor volume can now be controlled. This was not possible before.
    Now the individual sine waves are premixed.

--- Drift Car/Scenes/MotorSoundController.gd
+++ Drift Car/Scenes/MotorSoundController.gd
@@ -1,36 +1,30 @@
 extends Node
 
-@export var f1_node: NodePath
-@export var f2_node: NodePath
-@export var f3_node: NodePath
+@export var on_node: NodePath
+@export var off_node: NodePath
 
-var f1: AudioStreamPlayer3D
-var f2: AudioStreamPlayer3D
-var f3: AudioStreamPlayer3D
+var on: AudioStreamPlayer3D
+var off: AudioStreamPlayer3D
+
 var rev_min: float = 1
 var rev_multiplier: float = 6 # Higher values result in higher revs.
-var amplification_high: AudioEffectAmplify
-var amplification_mid: AudioEffectAmplify
-var amplification_low: AudioEffectAmplify
+var amplification_on: AudioEffectAmplify
+var amplification_off: AudioEffectAmplify
 var time_start: int
 var time_now: int
 var time_diff: float
 
 
 func _ready():
-	f1 = get_node(f1_node)
-	f2 = get_node(f2_node)
-	f3 = get_node(f3_node)
-	f1.play()
-	f2.play()
-	f3.play()
-	var index_low = AudioServer.get_bus_index("Low")
-	amplification_low = AudioServer.get_bus_effect(index_low, 0)
-	var index_mid = AudioServer.get_bus_index("Mid")
-	amplification_mid = AudioServer.get_bus_effect(index_mid, 0)
-	var index_high = AudioServer.get_bus_index("High")
-	amplification_high = AudioServer.get_bus_effect(index_high, 0)
+	on = get_node(on_node)
+	off = get_node(off_node)
+	on.play()
+	off.play()
 	time_start = Time.get_ticks_msec()
+	var index_amplifier_on = AudioServer.get_bus_index("On")
+	amplification_on = AudioServer.get_bus_effect(index_amplifier_on, 0)
+	var index_amplifier_off = AudioServer.get_bus_index("Off")
+	amplification_off = AudioServer.get_bus_effect(index_amplifier_off, 0)
 
 func _on_monteri_update_motor_sound(vehicle_controller):
 	var car = vehicle_controller
@@ -48,16 +42,13 @@ func _on_monteri_update_motor_sound(vehicle_controller):
 	else:
 		cut_off = false
 	if car.driver.did_accelerate && car.on_ground:
-		amplification_low.volume_db = 6
-		amplification_mid.volume_db = 6
 		if speed > v_cut_off && cut_off:
-			amplification_high.volume_db = -24
+			amplification_on.volume_db = -18
 		else:
-			amplification_high.volume_db = 0
+			amplification_on.volume_db = -6
+		amplification_off.volume_db = -18
 	else:
-		amplification_low.volume_db = 6
-		amplification_mid.volume_db = -3
-		amplification_high.volume_db = -20
-	f1.pitch_scale = pitch
-	f2.pitch_scale = pitch
-	f3.pitch_scale = pitch
+		amplification_on.volume_db = -24
+		amplification_off.volume_db = -10
+	on.pitch_scale = pitch
+	off.pitch_scale = pitch
