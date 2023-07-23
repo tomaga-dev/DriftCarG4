a5cd8d9f11559daedcac7b9fb2559e48147f339f
tomaga <tomaga1080@protonmail.com>
2023-07-23 09:01:40 +0200

    Motor sound update
    The rev variables are exported, so that you can adjust them from the
    editor. Calculating the cut-off time has been simpllfied.

--- Drift Car/Scenes/MotorSoundController.gd
+++ Drift Car/Scenes/MotorSoundController.gd
@@ -2,17 +2,19 @@ extends Node
 
 @export var on_node: NodePath
 @export var off_node: NodePath
+@export var rev_min: float = 1.6
+@export var rev_multiplier: float = 5.5 # Higher values result in higher revs.
 
 var on: AudioStreamPlayer3D
 var off: AudioStreamPlayer3D
 
-var rev_min: float = 1
-var rev_multiplier: float = 5 # Higher values result in higher revs.
 var amplification_on: AudioEffectAmplify
 var amplification_off: AudioEffectAmplify
+var on_volume: float
+var off_volume: float
 var time_start: int
 var time_now: int
-var time_diff: float
+var time_diff: int = 52 # milliseconds
 
 
 func _ready():
@@ -30,34 +32,40 @@ func _on_monteri_update_motor_sound(vehicle_controller):
 	var car = vehicle_controller
 	var cut_off: bool
 	var vmax: float = car.gearbox.get_vmax()
-	var v_cut_off = vmax * 0.94
+	var v_cut_off = vmax * 0.97
 	var speed: float = car.linear_velocity.length()
 	var gradient: float = rev_multiplier * speed / vmax
 	var pitch: float
 	time_now = Time.get_ticks_msec()
-	if time_now >= time_start + 105:
-		time_diff = 0.5 * (time_now - time_start)
+	if time_now >= time_start + 2 * time_diff:
 		time_start = time_now
 	if time_now >= time_start + time_diff:
 		cut_off = true
 	else:
 		cut_off = false
 	if car.on_ground:
-		if car.is_shifting():
-			amplification_on.volume_db = -40
-			amplification_off.volume_db = -20
-		else: if car.driver.did_accelerate:
-			if speed > v_cut_off && cut_off:
-				amplification_on.volume_db = -40
-			else:
-				amplification_on.volume_db = 0
-			amplification_off.volume_db = -40
-		else:
-			amplification_on.volume_db = -40
-			amplification_off.volume_db = -4 * gradient
+		update_sound(car, speed, v_cut_off, cut_off, gradient)
 	else:
-		amplification_on.volume_db = -40
-		amplification_off.volume_db = -20
+		on_volume = -40
+		off_volume = -20
+	amplification_on.volume_db = on_volume
+	amplification_off.volume_db = off_volume
 	pitch = rev_min + gradient
 	on.pitch_scale = pitch
 	off.pitch_scale = pitch
+
+func update_sound(car: VehicleController, speed: float, v_cut_off: float, cut_off: bool, gradient: float):
+	if car.gearbox.gear_changing:
+		on_volume = -40
+		off_volume = -20
+	else: if car.driver.did_accelerate:
+		if speed > v_cut_off && cut_off:
+			on_volume = -40
+		else:
+			on_volume = -3
+		off_volume = -40
+	else:
+		on_volume = -40
+		off_volume = -4 * gradient
+
+
