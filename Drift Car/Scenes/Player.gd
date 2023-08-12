bdbfc39889c0c8794946b326f5a7883a3f2b1ed2
tomaga <tomaga1080@protonmail.com>
2023-08-12 14:35:41 +0200

    A car can be assigned to the player

--- Drift Car/Scenes/Player.gd
+++ Drift Car/Scenes/Player.gd
@@ -1,8 +1,14 @@
 extends Node
 
+@export var car_node: NodePath
 
-func _on_monteri_query_driver(driver):
-	driver.start_query()
+var car: VehicleController
+
+
+func _ready():
+	car = get_node(car_node)
+
+func get_player_input(driver):
 	if Input.is_action_pressed("controller_brake"):
 		driver.brake()
 	else:
@@ -14,3 +20,11 @@ func _on_monteri_query_driver(driver):
 			driver.turn_right()
 		if Input.is_action_pressed("controller_reverse"):
 			driver.reverse()
+
+func query_driver(car_of_driver: VehicleController, _rev_normalized: float):
+	car_of_driver.driver.start_query()
+	if car_of_driver == car:
+		get_player_input(car_of_driver.driver)
+
+func _on_monteri_query_driver(car_of_driver, rev_normalized):
+	query_driver(car_of_driver, rev_normalized)
