9f5047ee07d8bdc95871fd2cc830b5c11bc4a427
tomaga <tomaga1080@protonmail.com>
2023-08-15 09:12:48 +0200

    A node path to the rev counter needle
    The rev counter needle will be rotated here.

--- Drift Car/Scenes/Player.gd
+++ Drift Car/Scenes/Player.gd
@@ -1,12 +1,21 @@
 extends Node
 
 @export var car_node: NodePath
+@export var needle_node: NodePath
 
 var car: VehicleController
+var needle: TextureRect
+var m: float
+var b: float
 
 
 func _ready():
 	car = get_node(car_node)
+	needle = get_node(needle_node)
+	var k = 0.5 * PI + PI
+	# max revolutions = 7 (thousand)
+	m = 6.2 / 7 * k
+	b = 0.8 / 7 * k
 
 func get_player_input(driver):
 	if Input.is_action_pressed("controller_brake"):
@@ -21,10 +30,11 @@ func get_player_input(driver):
 		if Input.is_action_pressed("controller_reverse"):
 			driver.reverse()
 
-func query_driver(car_of_driver: VehicleController, _rev_normalized: float):
+func query_driver(car_of_driver: VehicleController, rev_normalized: float):
 	car_of_driver.driver.start_query()
 	if car_of_driver == car:
 		get_player_input(car_of_driver.driver)
+		needle.rotation = -PI + m * rev_normalized + b
 
 func _on_monteri_query_driver(car_of_driver, rev_normalized):
 	query_driver(car_of_driver, rev_normalized)
